ab5fe215cd093f36d7b9555645faf27e945dee86
Panos  Patros <patrospanos@gmail.com>
2023-03-23 10:49:30 +1300

    Implementation of action, struct of params and simple test

---
+++ src/Serilog.Sinks.Raygun/Sinks/Raygun/OnBeforeSendParameters.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Serilog.Events;
+
+namespace Serilog.Sinks.Raygun
+{
+    public struct OnBeforeSendParameters
+    {
+        private readonly LogEvent _logEvent;
+        private readonly List<string> _tags;
+        private readonly Dictionary<string, LogEventPropertyValue> _properties;
+
+        public LogEvent LogEvent => _logEvent;
+        public List<string> Tags => _tags;
+        public Dictionary<string, LogEventPropertyValue> Properties => _properties;
+
+        public OnBeforeSendParameters(LogEvent logEvent, List<string> tags, Dictionary<string, LogEventPropertyValue> properties)
+        {
+            _logEvent = logEvent;
+            _tags = tags;
+            _properties = properties;
+        }
+    }
+}
---
+++ test/Serilog.Sinks.Raygun.Tests/Sinks/Raygun/OnBeforeSendActionTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Serilog.Sinks.Raygun.Tests.Sinks.Raygun
+{
+    [TestFixture]
+    public class OnBeforeSendActionTests
+    {
+        [Test]
+        public void TestCreatingRaygunSinkWithOnBeforeSendAction()
+        {
+            var raygunSink = new RaygunSink(
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    parameters => { }
+                );
+            
+            Assert.NotNull(raygunSink);
+        }
+        
+    }
+}
