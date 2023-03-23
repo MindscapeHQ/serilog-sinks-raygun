e8879235848a3debf2345e6d0fe8d1a47c9851b6
Panos  Patros <patrospanos@gmail.com>
2023-03-23 13:15:38 +1300

    Asserts OnBeforeSend Action is called by Emit

--- test/Serilog.Sinks.Raygun.Tests/Sinks/Raygun/OnBeforeSendActionTests.cs
+++ test/Serilog.Sinks.Raygun.Tests/Sinks/Raygun/OnBeforeSendActionTests.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Serilog.Events;
+using Serilog.Parsing;
 
 namespace Serilog.Sinks.Raygun.Tests.Sinks.Raygun
 {
@@ -9,21 +14,38 @@ namespace Serilog.Sinks.Raygun.Tests.Sinks.Raygun
         public void TestCreatingRaygunSinkWithOnBeforeSendAction()
         {
             var raygunSink = new RaygunSink(
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    parameters => { }
+                formatProvider: null,
+                applicationKey: "",
+                onBeforeSend: parameters => { }
                 );
             
             Assert.NotNull(raygunSink);
         }
-        
+
+        [Test]
+        public void TestOnBeforeSendActionCalledAfterEmit()
+        {
+            var onBeforeSendFlag = false;
+            var raygunSink = new RaygunSink(
+                formatProvider: null,
+                applicationKey: "",
+                onBeforeSend: parameters =>
+                {
+                    onBeforeSendFlag = true;
+                }
+            );
+            
+            Assert.NotNull(raygunSink);
+            Assert.IsFalse(onBeforeSendFlag);
+            
+            raygunSink.Emit(new LogEvent(
+                DateTimeOffset.Now,
+                LogEventLevel.Information,
+                null,
+                new MessageTemplate(Enumerable.Empty<MessageTemplateToken>()),
+                new List<LogEventProperty>()));
+            
+            Assert.IsTrue(onBeforeSendFlag);
+        }
     }
 }
