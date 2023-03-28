ce574c779e80672a65f1ef843c30be6cc78769e5
Panos  Patros <patrospanos@gmail.com>
2023-03-29 10:29:28 +1300

    Set up a basic end-to-end testing app. Sends stuff to Raygun ok
    WARNING: There are upcoming breaking changes with Serilog v3, currently in prerelease. I installed that one in the beginning and the system couldn't find teh `Sink` method we use in our extension method. We should expect issues to creep up once v3 is released

---
+++ test/Serilog.Sinks.Raygun.EndToEndTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using Serilog;
+using Serilog.Events;
+
+Console.Out.WriteLine("Please enter your Raygun API key: ");
+var raygunApiKey = Console.In.ReadLine();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .WriteTo.Raygun(raygunApiKey,
+        null,
+        "CustomUserNameProperty",
+        "CustomAppVersionProperty",
+        LogEventLevel.Information,
+        null,
+        new[] { "globalTag1", "globalTag2" },
+        new[] { "ignoreField1", "ignoreField2" },
+        "CustomGroupKeyProperty",
+        "CustomTagsProperty",
+        "CustomUserInfoProperty",
+        onBeforeSendArguments =>
+        {
+            Console.Out.WriteLine("OnBeforeSend called with the following arguments: " + onBeforeSendArguments);
+            //Updating machine name
+            onBeforeSendArguments.RaygunMessage.Details.MachineName = "Serilog.Sinks.Raygun.EndToEndTest Machine";
+            
+            //Testing Logging in Logger callback. Uncomment to cause a stack overflow
+            //Log.Fatal("Recursive error");
+            
+            //Testing throwing an exception in Action
+            throw new Exception("Exception thrown in callback action..");
+            
+        })
+    .CreateLogger();
+
+try
+{
+    throw new Exception("Serilog.Sinks.Raygun.EndToEndTest");
+}
+catch (Exception e)
+{
+    Console.Out.WriteLine("Sending message to Raygun");
+    Log.Fatal(e, "Logging error");
+}
+
+throw new Exception("Unhandled Serilog.Sinks.Raygun.EndToEndTest exception");
